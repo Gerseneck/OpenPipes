4ec46208492175e5e40f667d1589a35444e9905a
Gerseneck <wang14240@gmail.com>
2023-05-12 12:16:56 -0400

    added another level

--- data/level.py
+++ data/level.py
@@ -11,7 +11,7 @@ LEVELS = {
             ((0, 2), color.green), ((3, 1), color.green),
             ((1, 2), color.blue), ((4, 2), color.blue),
             ((0, 4), color.yellow), ((3, 3), color.yellow),
-            ((1, 4), color.orange), ((4, 3), color.orange)
+            ((1, 4), color.orange), ((4, 3), color.orange),
         ],
     },
     1: {
@@ -23,8 +23,8 @@ LEVELS = {
             ((0, 3), color.green), ((4, 3), color.green),
             ((0, 2), color.blue), ((4, 0), color.blue),
             ((3, 0), color.yellow), ((0, 1), color.yellow),
-            ((3, 3), color.orange), ((4, 2), color.orange)
-        ]
+            ((3, 3), color.orange), ((4, 2), color.orange),
+        ],
     },
     2: {
         'name': '5x5 Level 2',
@@ -34,15 +34,18 @@ LEVELS = {
             ((0, 0), color.red), ((4, 0), color.red),
             ((2, 3), color.green), ((4, 2), color.green),
             ((0, 4), color.blue), ((4, 4), color.blue),
-            ((1, 2), color.yellow), ((4, 3), color.yellow)
-        ]
+            ((1, 2), color.yellow), ((4, 3), color.yellow),
+        ],
     },
     3: {
-        'name': '7x7 Level 1',
-        'size': 7,
-        'time': 10,
+        'name': '7x7 Level 1', 
+        'size': 7, 
         'nodes': [
-
-        ]
-    }
+            ((1, 0), color.blue), ((6, 0), color.blue), 
+            ((4, 2), color.green), ((5, 5), color.green), 
+            ((1, 1), color.red), ((4, 1), color.red), 
+            ((2, 4), color.orange), ((5, 4), color.orange), 
+            ((2, 0), color.yellow), ((2, 3), color.yellow),
+        ],
+    },
 }
